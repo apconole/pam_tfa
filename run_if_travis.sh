6bb0bbb62bc5872343f73ca6244938e0f353b3c6
Aaron Conole <aaron@bytheb.org>
2015-07-19 20:48:48 -0400

    Add cppcheck for jenkins side builds

--- run_if_travis.sh
+++ run_if_travis.sh
@@ -5,4 +5,5 @@ if [ "X$TRAVIS_JENKINS_RESULT" == "X" ]; then coveralls --gcov-options '\-lp'; f
 if [ "X$TRAVIS_JENKINS_RESULT" != "X" ]; then
     lcov --capture --base-directory . --directory . -o Results.lcov
     gcovr -r . -x -o code_coverage.xml
+    cppcheck --enable=all --xml . -I. 2>cppcheck.cxml
 fi
